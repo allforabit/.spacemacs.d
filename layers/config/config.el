513dfd31261887d6ccec97885dc0bf483ae282d8
Kevin Nolan <kevnolan@gmail.com>
2017-10-02 15:51:21 +0100

    c:/Users/admin/Documents/GitHub/spacemacs.d/layers/config/config.el

--- layers/config/config.el
+++ layers/config/config.el
@@ -1,1 +1,6 @@
-(setenv "PATH" "C:\\msys2\\mingw64\\bin;C:\\msys2\\usr\\local\\bin;C:\\msys2\\usr\\bin;C:\\Windows\\System32;C:\\Windows")
+;; -*- eval: (git-auto-commit-mode 1) -*-
+;;; Setup path
+;; Setting path here for now to make sure msys2 is used
+;; May not be neccessary, but may be a clearer more explicit approach
+;; https://www.onwebsecurity.com/configuration/zsh-shell-inside-emacs-on-windows.html
+;; (setenv "PATH" "C:\\mingw64\\bin;C:\\mingw64\\usr\\local\\bin;C:\\mingw64\\usr\\bin;C:\\Windows\\System32;C:\\Windows")
